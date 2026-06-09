d4fc8af49122939ca82c96a1dc6e4cb9aa41f13a
Muhammad Jahangir <jahangir.2012@outlook.com>
2026-06-09 23:25:34 +0600

    sys ip added

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     server:{
       proxy:{
           '/api/':{
-              target: 'http://localhost:5000'
+              target: 'http://51.75.173.221:5000'
           }
       }
     }
